d2248ce7ae1a8b8151fa0988e15714bc545a6f9f
StephenGletse <gletsekwame17@gmail.com>
2023-03-13 15:45:42 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container:{
+      padding:'1rem',
+      center:true
+    },
+    extend: {},
+  },
+  plugins: [],
+}
